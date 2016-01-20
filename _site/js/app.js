f49497f08bd8a7d5c46126879f6433a439376b1c
jayrenn <joshrenn@microsoft.com>
2016-01-20 15:17:07 -0800

    update create HWA page

--- _site/js/app.js
+++ _site/js/app.js
@@ -4,5 +4,41 @@
 
     var filename = window.location.pathname.split('/').pop();
     $("a[href*='" + filename + "'] h3").toggleClass('active');
-    
+
+    var platformToggle = $("[name=platform]");
+    var prevPlatform = platformToggle[0].checked ? platformToggle[0].value : platformToggle[1].value;
+
+    if (window.location.hash) {
+        var hash = window.location.hash.substring(1);
+
+        if (hash != prevPlatform) {
+            if (hash == platformToggle[0].value) {
+                platformToggle[0].checked = true;
+                platformToggle[1].checked = false;
+                prevPlatform = hash;
+            }
+
+            if (hash == platformToggle[1].value) {
+                platformToggle[1].checked = true;
+                platformToggle[0].checked = false;
+                prevPlatform = hash;
+            }
+        }
+    }
+
+    $("#reqs-" + prevPlatform).show();
+    $("#steps-" + prevPlatform).show();
+
+    platformToggle.click(function () {
+    	var curPlatform = $(this).val();
+
+    	if (curPlatform != prevPlatform) {
+    		$(".reqs").hide();
+    		$(".steps").hide();
+    		$("#reqs-" + curPlatform).show();
+    		$("#steps-" + curPlatform).show();
+    		prevPlatform = curPlatform;
+            window.location = "#" + curPlatform;
+    	}
+    });
 });
